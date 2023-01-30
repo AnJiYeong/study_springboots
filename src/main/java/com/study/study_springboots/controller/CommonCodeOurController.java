f7706235745bd17c4b1a9f9b4433373409b09f16
AnJiYeong <memarcis2@naver.com>
2023-01-30 15:23:45 +0900

    230130

--- src/main/java/com/study/study_springboots/controller/CommonCodeOurController.java
+++ src/main/java/com/study/study_springboots/controller/CommonCodeOurController.java
@@ -172,6 +172,17 @@ public class CommonCodeOurController {
         return modelAndView;
     }
 
+    @RequestMapping(value = { "/listPagination/{currentPage}"}, method = RequestMethod.GET)
+    public ModelAndView listPagination(@RequestParam Map<String, Object> params, ModelAndView modelAndView
+            , @PathVariable String currentPage) {
+        params.put("currentPage", Integer.parseInt(currentPage));
+        params.put("pageScale", 10);
+        Object resultMap = commonCodeOurService.getListWithPagination(params);
+        modelAndView.addObject("resultMap", resultMap);
+        modelAndView.setViewName("commonCode_our/list_pagination");
+        return modelAndView;
+    }
+
     @RequestMapping(value = { "/edit/{uniqueId}" }, method = RequestMethod.GET)
     public ModelAndView edit(@RequestParam Map<String, Object> params, @PathVariable String uniqueId,
             ModelAndView modelAndView) {
